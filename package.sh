b3d4449499e4c6bdc907b2bd271af4f4f9579580
Dan Garrette <dhgarrette@gmail.com>
2014-11-11 13:08:21 -0600

    added package.sh script

---
+++ package.sh
@@ -0,0 +1,3 @@
+sbt package;    mv target/junto-1.6.1-SNAPSHOT.jar         ../low-resource-pos-tagging-2014/lib
+sbt packageSrc; mv target/junto-1.6.1-SNAPSHOT-sources.jar ../low-resource-pos-tagging-2014/lib
+
